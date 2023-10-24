f9e0239260a7b0d308697d8cccec23592afdfe50
Kenny Chu <kennychu@asl.com.hk>
2023-10-24 11:27:23 +0800

    set up project

---
+++ src/app/providers.tsx
@@ -0,0 +1,32 @@
+// app/providers.tsx
+'use client'
+
+import { CacheProvider } from '@chakra-ui/next-js'
+import { ChakraProvider } from '@chakra-ui/react'
+import { extendTheme } from "@chakra-ui/react"
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      blue: '#4062BB',
+      primary: '#52489C',
+      cyan: '#59C3C3',
+      grey: '#EBEBEB',
+      red: '#F45B69'
+    }
+  }
+})
+
+export function Providers({ 
+    children 
+  }: { 
+  children: React.ReactNode 
+  }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider theme={theme}>
+        {children}
+      </ChakraProvider>
+    </CacheProvider>
+  )
+}
